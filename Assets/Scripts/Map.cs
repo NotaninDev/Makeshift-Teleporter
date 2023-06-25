89a2471d91716b35a29c0d5e4de27c5160616462
Notan <49018674+NotaninDev@users.noreply.github.com>
2023-06-25 15:59:47 +0900

    Implement bare-minimum block move animation

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -14,11 +14,13 @@ public class Map : MonoBehaviour
 
     private GameObject playerObject, targetObject, tileParent, blockParent;
     private SpriteBox playerSprite, targetSprite;
-    private Dictionary<Vector2Int, GameObject> blocks;
+    private GameObject[,] blockObjects;
+    private Stack<GameObject> blockPool;
 
     private const float MoveTime = .1f, StuckTime = .1f;
     private const float StuckScale = 1f / StuckTime / StuckTime;
-    private IEnumerator moveAnimation;
+    private IEnumerator playerAnimation, blockGroupAnimation;
+    private GameObject blockGroupParent;
 
     // Scale is the scale of the map
     public float Scale { get; private set; }
@@ -43,7 +45,8 @@ public class Map : MonoBehaviour
         targetSprite = targetObject.AddComponent<SpriteBox>();
         tileParent = General.AddChild(gameObject, "Tile Parent");
         blockParent = General.AddChild(gameObject, "Block Parent");
-        blocks = new Dictionary<Vector2Int, GameObject>();
+        blockGroupParent = General.AddChild(gameObject, "Block Group Parent");
+        blockPool = new Stack<GameObject>();
     }
 
     public void Initialize(MapData mapData)
@@ -66,6 +69,8 @@ public class Map : MonoBehaviour
         transform.localScale = Vector3.one * Scale;
         transform.localPosition = Vector3.zero;
 
+        blockObjects = new GameObject[mapData.Size.x, mapData.Size.y];
+        int blockCount = 0;
         for (int i = 0; i < mapData.Size.x; i++)
         {
             for (int j = 0; j < mapData.Size.y; j++)
@@ -80,11 +85,11 @@ public class Map : MonoBehaviour
                 Vector2Int coordinates = new Vector2Int(i, j);
                 if (mapData.HasBlock(i, j))
                 {
-                    GameObject blockObject = General.AddChild(blockParent, $"Block ({i}, {j})");
-                    blocks[coordinates] = blockObject;
-                    Block block = blockObject.AddComponent<Block>();
-                    block.Initialize(!mapData.IsConnected(coordinates, MapData.Direction.Up), !mapData.IsConnected(coordinates, MapData.Direction.Right), !mapData.IsConnected(coordinates, MapData.Direction.Down), !mapData.IsConnected(coordinates, MapData.Direction.Left));
-                    blockObject.transform.localPosition = Get3DPoint(i, j);
+                    blockCount++;
+                    blockObjects[i, j] = General.AddChild(blockParent, $"Block {blockCount}");
+                    Block tempBlock = blockObjects[i, j].AddComponent<Block>();
+                    tempBlock.Initialize(!mapData.IsConnected(coordinates, MapData.Direction.Up), !mapData.IsConnected(coordinates, MapData.Direction.Right), !mapData.IsConnected(coordinates, MapData.Direction.Down), !mapData.IsConnected(coordinates, MapData.Direction.Left));
+                    blockObjects[i, j].transform.localPosition = Get3DPoint(i, j);
                 }
             }
         }
@@ -102,8 +107,35 @@ public class Map : MonoBehaviour
     public float Move(MapData previousMap, MapData.Direction direction)
     {
         bool stuck = mapData.Player == previousMap.Player;
-        moveAnimation = stuck ? AnimateStuck() : Graphics.Move(playerObject, Get3DPoint(previousMap.Player), Get3DPoint(mapData.Player), MoveTime);
-        if (moveAnimation != null) StartCoroutine(moveAnimation);
+        playerAnimation = stuck ? AnimateStuck() : Graphics.Move(playerObject, Get3DPoint(previousMap.Player), Get3DPoint(mapData.Player), MoveTime);
+        if (playerAnimation != null) StartCoroutine(playerAnimation);
+
+        Dictionary<Vector2Int, GameObject> movedBlockObjects = new Dictionary<Vector2Int, GameObject>();
+        if (mapData.BlockGroupMoved)
+        {
+            blockGroupParent.transform.localPosition = Get3DPoint(previousMap.Player);
+            for (int i = 0; i < mapData.Size.x; i++)
+            {
+                for (int j = 0; j < mapData.Size.y; j++)
+                {
+                    if (mapData.BlockMoved[i, j])
+                    {
+                        Vector2Int coordinates = new Vector2Int(i, j);
+                        movedBlockObjects[coordinates] = blockObjects[i, j];
+                        blockObjects[i, j] = null;
+                        movedBlockObjects[coordinates].transform.SetParent(blockGroupParent.transform);
+                    }
+                }
+            }
+            foreach (KeyValuePair<Vector2Int, GameObject> pair in movedBlockObjects)
+            {
+                int x = pair.Key.x,
+                    y = pair.Key.y;
+                blockObjects[x, y] = pair.Value;
+            }
+            blockGroupAnimation = Graphics.Move(blockGroupParent, Get3DPoint(previousMap.Player), Get3DPoint(mapData.Player), MoveTime);
+            StartCoroutine(blockGroupAnimation);
+        }
         return stuck ? StuckTime : MoveTime;
     }
 
@@ -128,12 +160,47 @@ public class Map : MonoBehaviour
         playerObject.transform.localPosition = Get3DPoint(mapData.Player);
         playerObject.transform.localScale = Vector3.one;
     }
+    public void MoveBlock(int x, int y)
+    {
+        //
+        // to do: implement this lol
+        //
+        blockObjects[x, y].transform.localPosition = Get3DPoint(x, y);
+    }
 
     public void StopAnimation()
     {
-        if (moveAnimation != null) StopCoroutine(moveAnimation);
+        if (playerAnimation != null) StopCoroutine(playerAnimation);
+        if (blockGroupAnimation != null) StopCoroutine(blockGroupAnimation);
 
         // update objects' position
         MovePlayer();
+
+        while (blockGroupParent.transform.childCount > 0)
+        {
+            blockGroupParent.transform.GetChild(0).SetParent(blockParent.transform);
+        }
+        for (int i = 0; i < mapData.Size.x; i++)
+        {
+            for (int j = 0; j < mapData.Size.y; j++)
+            {
+                if (blockObjects[i, j] != null)
+                {
+                    blockPool.Push(blockObjects[i, j]);
+                    blockObjects[i, j] = null;
+                }
+            }
+        }
+        for (int i = 0; i < mapData.Size.x; i++)
+        {
+            for (int j = 0; j < mapData.Size.y; j++)
+            {
+                if (mapData.HasBlock(i, j))
+                {
+                    blockObjects[i, j] = blockPool.Pop();
+                    MoveBlock(i, j);
+                }
+            }
+        }
     }
 }
